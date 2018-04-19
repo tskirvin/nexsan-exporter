2b581758449c4ee12ede0fa80b49a8a41e1ef632
Sam Morris <sam@robots.org.uk>
2018-04-19 15:50:57 +0100

    Initial metric collection from probed data

--- nexsan_exporter/exporter.py
+++ nexsan_exporter/exporter.py
@@ -28,14 +28,27 @@ def front(environ, start_response):
     with io.BytesIO() as page:
         page.write(
             b'<html>'
-                b'<head><title>Nexsan exporter</title></head>'
+                b'<head>'
+                    b'<title>Nexsan exporter</title>'
+                    b'<style>'
+                        b'form { display: grid; grid-template-columns: 175px 175px; grid-gap: 16px; }'
+                        b'label { grid-column: 1 / 2; text-align: right; }'
+                        b'input, button { grid-column: 2 / 3; }'
+                    b'</style>'
+                b'</head>'
                 b'<body>'
                     b'<h1>Nexsan Exporter</h1>'
+                    b'<p>Use this form to probe an array:'
                     b'<form method="get" action="/probe">'
-                        b'<p><label for="target">Probe address:</label>'
-                            b'<input type="text" name="target" placeholder="192.0.2.1">'
-                            b'<button type="submit">Probe</button>'
+                        b'<label for="target">Probe address:</label>'
+                        b'<input type="text" id="target" name="target" required placeholder="192.0.2.1">'
+                        b'<label for="user">User:</label>'
+                        b'<input type="text" id="user" name="user" required placeholder="admin">'
+                        b'<label for="pass">Password:</label>'
+                        b'<input type="password" required id="pass" name="pass">'
+                        b'<button type="submit">Probe</button>'
                     b'</form>'
+                    b'<hr>'
                     b'<p><a href="/metrics">Metrics</a>'
                 b'</body>'
             b'</html>'
@@ -51,7 +64,7 @@ def probe(environ, start_response):
     qs = urllib.parse.parse_qs(environ['QUERY_STRING'])
 
     reg = prometheus_client.CollectorRegistry()
-    reg.register(nexsan.probe(qs['target'][0]))
+    reg.register(nexsan.probe(target=qs['target'][0], user=qs['user'][0], pass_=qs['pass'][0]))
     body = prometheus_client.generate_latest(reg)
 
     start_response('200 OK', [('Content-Type', prometheus_client.CONTENT_TYPE_LATEST)])
--- nexsan_exporter/nexsan.py
+++ nexsan_exporter/nexsan.py
@@ -1,12 +1,39 @@
-def probe(target):
+import base64
+import prometheus_client
+import traceback
+import urllib.request
+import urllib.parse
+
+from xml.etree import ElementTree
+
+import prometheus_client
+
+def probe(target, user, pass_):
     '''
     Returns a collector populated with metrics from the target array.
     '''
-    return Collector(target)
+    auth = b'Basic ' + base64.b64encode('{}:{}'.format(user, pass_).encode('latin1'))
+    url = urllib.parse.urlunsplit(('http', target, '/admin/opstats.asp', None, None))
+    req = urllib.request.Request(url)
+    req.add_header(b'Authorization', auth)
+
+    return Collector(req)
 
 class Collector:
-    def __init__(self, target):
-        self.__target = target
+    def __init__(self, req):
+        self.__req = req
 
     def collect(self):
-        raise StopIteration
+        with urllib.request.urlopen(self.__req, timeout=5) as stats_f:
+            stats = ElementTree.parse(stats_f)
+            sys_details = stats.find('nexsan_sys_details')
+            if sys_details:
+                yield from self.collect_sys_details(sys_details)
+
+    def collect_sys_details(self, sys_details):
+        labels = ['friendly_name', 'system_name', 'system_id', 'firmware_version', 'date']
+        values = []
+        for l in labels:
+            values.append(sys_details.findtext(label))
+        g = prometheus_client.core.GaugeMetricFamily('nexsan_sys_details', labels)
+        g.add_metric(values, 1)
