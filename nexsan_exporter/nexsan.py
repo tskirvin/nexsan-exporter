eaed2a56c3ea24ae2a147376e73c8a970862c880
Sam Morris <sam@robots.org.uk>
2018-04-23 14:56:01 +0100

    Add more PSU metrics

--- nexsan_exporter/nexsan.py
+++ nexsan_exporter/nexsan.py
@@ -22,48 +22,49 @@ def probe(target, user, pass_):
 class Collector:
     def __init__(self, opstats):
         self.__opstats = opstats
+        self.__parent_map = {c: p for p in opstats.iter() for c in p}
+
+    def isgood(self, elem):
+        if elem.attrib['good'] == 'yes':
+            return 1
+        else:
+            return 0
 
     def collect(self):
-        sys_details = self.__opstats.find('nexsan_sys_details')
-        if sys_details:
-            yield from self.collect_sys_details(sys_details)
-        env_status = self.__opstats.find('nexsan_env_status')
-        if env_status:
-            yield from self.collect_env_status(env_status)
+        for child in self.__opstats.iterfind('.//*'):
+            if child.tag == 'nexsan_sys_details':
+                yield from self.collect_sys_details(child)
+            elif child.tag == 'nexsan_env_status':
+                yield from self.collect_env_status(child)
 
     def collect_sys_details(self, sys_details):
         labels = ['friendly_name', 'system_name', 'system_id', 'firmware_version']
-        values = []
-        for l in labels:
-            values.append(sys_details.findtext(l))
-
         g = prometheus_client.core.UntypedMetricFamily('nexsan_sys_details', '', labels=labels)
-        g.add_metric(values, 1)
+        g.add_metric([sys_details.findtext(l) for l in labels], 1)
         yield g
 
         yield prometheus_client.core.CounterMetricFamily('nexsan_sys_date', '', int(sys_details.findtext('date')))
 
     def collect_env_status(self, env_status):
-        for e in env_status.iterfind('enclosure'):
-            yield from self.collect_enclosure(e)
+        for p in env_status.iterfind('.//psu'):
+            yield from self.collect_psu(p)
 
-    def collect_enclosure(self, enclosure):
-        for p in enclosure.iterfind('psu'):
-            for id_, good in self.collect_psu_good(psu):
-                yield from self.collect_psu(p)
+    def collect_psu(self, psu):
+        labels = ['psu', 'enclosure']
+        values = [psu.attrib['id'], self.__parent_map[psu].attrib['id']]
 
-    def collect_psu_good(self, psu):
-        yield (psu.get('id'), 1 if psu.get('attrib') == yes else 0)
-        #
-        g = prometheus_client.core.UntypedMetricFamily('nexsan_env_psu_good', '', labels=['psu_id', 'enclosure_id'])
-        g.add_metric(1 if psu.get('attrib') == 'yes' else 0)
-        yield g
+        g1 = prometheus_client.core.GaugeMetricFamily('nexsan_env_psu_power_good', '', labels=labels)
+        g1.add_metric(values, self.isgood(psu.find('state')))
+        yield g1
 
+        g2 = prometheus_client.core.GaugeMetricFamily('nexsan_env_psu_power_watts', '', labels=labels)
+        g2.add_metric(values, int(psu.find('state').attrib['power_watt']))
+        yield g2
 
+        g3 = prometheus_client.core.GaugeMetricFamily('nexsan_env_psu_temp_celsius', '', labels=labels)
+        g3.add_metric(values, int(psu.find('temperature_deg_c').text))
+        yield g3
 
-# blah
-        nexsan_env_psu_good
-        nexsan_env_psu_power_watts
-        nexsan_env_psu_temp_c
-        nexsan_env_psu_blower_rpm
-        raise StopIteration
+        g4 = prometheus_client.core.GaugeMetricFamily('nexsan_env_psu_temp_good', '', labels=labels)
+        g4.add_metric(values, self.isgood(psu.find('temperature_deg_c')))
+        yield g4
--- test/test_nexsan.py
+++ test/test_nexsan.py
@@ -1,9 +1,21 @@
 import os
-from xml.etree import ElementTree
+from xml.etree import ElementTree as ET
 
 import pytest
 
 from nexsan_exporter import nexsan
+
+def test_attrib_good():
+    elem = ET.Element('a')
+    elem.attrib['good'] = 'yes'
+    c = nexsan.Collector(elem)
+    assert 1 == c.isgood(elem)
+
+def test_attrib_bad():
+    elem = ET.Element('a')
+    elem.attrib['good'] = 'no'
+    c = nexsan.Collector(elem)
+    assert 0 == c.isgood(elem)
 
 @pytest.fixture
 def datadir(request):
@@ -19,7 +31,7 @@ def datadir(request):
 @pytest.mark.xfail
 def test_collector_opstats1(datadir):
     with open(os.path.join(datadir, 'opstats1.xml')) as f:
-        metrics = list(nexsan.Collector(ElementTree.parse(f)).collect())
+        metrics = list(nexsan.Collector(ET.parse(f)).collect())
         from pprint import pprint
         pprint(list(m.samples for m in metrics))
         assert [] == metrics
@@ -27,7 +39,7 @@ def test_collector_opstats1(datadir):
 @pytest.mark.xfail
 def test_collector_opstats2(datadir):
     with open(os.path.join(datadir, 'opstats2.xml')) as f:
-        metrics = list(nexsan.Collector(ElementTree.parse(f)).collect())
+        metrics = list(nexsan.Collector(ET.parse(f)).collect())
         from pprint import pprint
         pprint(list(m.samples for m in metrics))
         assert [] == metrics
@@ -43,7 +55,7 @@ def getmf(families, name):
 
 @pytest.fixture
 def nexsan_sys(request):
-    return ElementTree.fromstring('''
+    return ET.fromstring('''
         <nexsan_op_status version="2" status="experimental">
           <nexsan_sys_details version="1" status="experimental">
             <friendly_name>nnn</friendly_name>
@@ -66,3 +78,61 @@ def test_collector_sys_date(nexsan_sys):
     mf = getmf(c.collect(), 'nexsan_sys_date')
     assert 'counter' == mf.type
     assert [('nexsan_sys_date', {}, 1523963221)] == mf.samples
+
+@pytest.fixture
+def nexsan_psu(request):
+    return ET.fromstring('''
+      <nexsan_op_status version="2" status="experimental">
+        <nexsan_env_status version="3" status="experimental">
+          <enclosure id="1">
+            <psu id="2">
+              <state good="yes" power_watt="546">OK</state>
+              <temperature_deg_c good="yes">41</temperature_deg_c>
+              <blower_rpm id="0" good="yes">14438</blower_rpm>
+              <blower_rpm id="1" good="yes">14210</blower_rpm>
+              <blower_rpm id="2" good="yes">14210</blower_rpm>
+              <blower_rpm id="3" good="yes">14210</blower_rpm>
+            </psu>
+          </enclosure>
+        </nexsan_env_status>
+      </nexsan_op_status>
+    ''')
+
+def test_collector_psu_power_good_1(nexsan_psu):
+    c = nexsan.Collector(nexsan_psu)
+    mf = getmf(c.collect(), 'nexsan_env_psu_power_good')
+    assert 'gauge' == mf.type
+    assert [('nexsan_env_psu_power_good', {'psu': '2', 'enclosure': '1'}, 1)] == mf.samples
+
+def test_collector_psu_power_good_0(nexsan_psu):
+    nexsan_psu.find('.//psu/state').attrib['good'] = 'no'
+    c = nexsan.Collector(nexsan_psu)
+    mf = getmf(c.collect(), 'nexsan_env_psu_power_good')
+    assert 'gauge' == mf.type
+    assert [('nexsan_env_psu_power_good', {'psu': '2', 'enclosure': '1'}, 0)] == mf.samples
+
+def test_collector_psu_power_watts(nexsan_psu):
+    c = nexsan.Collector(nexsan_psu)
+    mf = getmf(c.collect(), 'nexsan_env_psu_power_watts')
+    assert 'gauge' == mf.type
+    assert [('nexsan_env_psu_power_watts', {'psu': '2', 'enclosure': '1'}, 546)] == mf.samples
+
+def test_collector_psu_temp_celsius(nexsan_psu):
+    c = nexsan.Collector(nexsan_psu)
+    mf = getmf(c.collect(), 'nexsan_env_psu_temp_celsius')
+    assert 'gauge' == mf.type
+    assert [('nexsan_env_psu_temp_celsius', {'psu': '2', 'enclosure': '1'}, 41)] == mf.samples
+
+def test_collector_psu_temp_good_1(nexsan_psu):
+    c = nexsan.Collector(nexsan_psu)
+    mf = getmf(c.collect(), 'nexsan_env_psu_temp_good')
+    assert 'gauge' == mf.type
+    assert [('nexsan_env_psu_temp_good', {'psu': '2', 'enclosure': '1'}, 1)] == mf.samples
+
+def test_collector_psu_temp_good_0(nexsan_psu):
+    nexsan_psu.find('.//psu/temperature_deg_c').attrib['good'] = 'no'
+    c = nexsan.Collector(nexsan_psu)
+    mf = getmf(c.collect(), 'nexsan_env_psu_temp_good')
+    assert 'gauge' == mf.type
+    assert [('nexsan_env_psu_temp_good', {'psu': '2', 'enclosure': '1'}, 0)] == mf.samples
+
